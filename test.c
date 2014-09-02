5d968e0325132040118e71f556282276c1c6d1f6
ZhangZhenxing <18660251039@163.com>
2014-09-02 18:40:02 +0800

    add c test file

---
+++ test.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  c test file
+ *
+ *        Version:  1.0
+ *        Created:  09/02/2014 06:30:30 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  ZhangZhenxing 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int mai(void)
+{
+	printf("hello world\n");
+	return 0;
+}
+
